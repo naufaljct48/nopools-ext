d67527395caf794fb75e89f60b227a927c68aad3
naufaljct48 <naufaljct48@gmail.com>
2023-07-09 04:33:11 +0000

    Deploying to gh-pages from @ naufaljct48/nopools-ext@07195b440bd2862fff28aa5d813e0ece1636a4d3 🚀

--- mangastream/Kiryuu/source.js
+++ mangastream/Kiryuu/source.js
@@ -1440,7 +1440,7 @@ const types_1 = require("@paperback/types");
 const MangaStream_1 = require("../MangaStream");
 const DOMAIN = 'https://kiryuu.id';
 exports.KiryuuInfo = {
-    version: (0, MangaStream_1.getExportVersion)('0.0.3'),
+    version: (0, MangaStream_1.getExportVersion)('0.0.4'),
     name: 'Kiryuu',
     description: `Extension that pulls manga from ${DOMAIN}`,
     author: 'NaufalJCT48',
