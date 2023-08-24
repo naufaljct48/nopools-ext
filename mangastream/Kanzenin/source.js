224067c50c6d579b46dbd37d13dc183711b5a129
naufaljct48 <naufaljct48@gmail.com>
2023-08-24 16:00:54 +0000

    Deploying to gh-pages from @ naufaljct48/nopools-ext@a1b3e0ed5d7ea0e97904132eb2534f235ea5fc96 🚀

--- mangastream/Kanzenin/source.js
+++ mangastream/Kanzenin/source.js
@@ -1440,7 +1440,7 @@ const types_1 = require("@paperback/types");
 const MangaStream_1 = require("../MangaStream");
 const DOMAIN = 'https://kanzenin.info';
 exports.KanzeninInfo = {
-    version: (0, MangaStream_1.getExportVersion)('0.0.0'),
+    version: (0, MangaStream_1.getExportVersion)('0.0.1'),
     name: 'Kanzenin',
     description: `Extension that pulls manga from ${DOMAIN}`,
     author: 'NaufalJCT48',
