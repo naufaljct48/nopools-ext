7dd06940ef3cb437ad5fd2dfa21a2b143ae31620
Naufal <naufaljct48@gmail.com>
2023-08-25 11:26:57 +0700

    Add WorldManhwas

---
+++ src/WorldManhwas/WorldManhwas.ts
@@ -0,0 +1,42 @@
+import {
+  ContentRating,
+  SourceInfo,
+  BadgeColor,
+  SourceIntents,
+} from "@paperback/types";
+
+import { getExportVersion, Madara } from "../Madara";
+
+const DOMAIN = "https://worldmanhwas.bar";
+
+export const ShinigamiInfo: SourceInfo = {
+  version: getExportVersion("0.0.0"),
+  name: "WorldManhwas",
+  description: `Extension that pulls manga from ${DOMAIN}`,
+  author: "NaufalJCT48",
+  authorWebsite: "http://github.com/NaufalJCT48",
+  icon: "icon.png",
+  contentRating: ContentRating.EVERYONE,
+  websiteBaseURL: DOMAIN,
+  sourceTags: [
+    {
+      text: "Indonesian",
+      type: BadgeColor.GREY,
+    },
+  ],
+  intents:
+    SourceIntents.MANGA_CHAPTERS |
+    SourceIntents.HOMEPAGE_SECTIONS |
+    SourceIntents.CLOUDFLARE_BYPASS_REQUIRED |
+    SourceIntents.SETTINGS_UI,
+};
+
+export class Shinigami extends Madara {
+  baseUrl: string = DOMAIN;
+
+  override alternativeChapterAjaxEndpoint = true;
+
+  override hasAdvancedSearchPage = true;
+
+  override directoryPath = "komik";
+}
