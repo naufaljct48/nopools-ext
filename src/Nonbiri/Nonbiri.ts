2482dfa9afcb29a5ab5788dd324bc9684a52cf61
Naufal <naufaljct48@gmail.com>
2023-10-09 23:43:51 +0700

    Update Nonbiri.ts

--- src/Nonbiri/Nonbiri.ts
+++ src/Nonbiri/Nonbiri.ts
@@ -13,7 +13,7 @@ import {
 const DOMAIN = 'https://nonbiri.space'
 
 export const NonbiriInfo: SourceInfo = {
-    version: getExportVersion('0.0.0'),
+    version: getExportVersion('0.0.1'),
     name: 'Nonbiri',
     description: `Extension that pulls manga from ${DOMAIN}`,
     author: 'NaufalJCT48',
