c8de08626d8301fbc12a481d53e04eef840f3e43
naufaljct48 <naufaljct48@gmail.com>
2024-11-15 16:46:29 +0000

    Deploying to gh-pages from @ naufaljct48/nopools-ext@806e905b3031e30fe4ac9ca31c824400e2e2ae04 🚀

--- madara/Shinigami/source.js
+++ madara/Shinigami/source.js
@@ -16191,7 +16191,7 @@ Please go to the homepage of <${this.baseUrl}> and press the cloud icon.`);
   // src/Shinigami/Shinigami.ts
   var DOMAIN = "https://shinigami07.com";
   var ShinigamiInfo = {
-    version: getExportVersion("0.0.8"),
+    version: getExportVersion("0.0.9"),
     name: "Shinigami",
     description: `Extension that pulls manga from ${DOMAIN}`,
     author: "NaufalJCT48",
@@ -16213,7 +16213,7 @@ Please go to the homepage of <${this.baseUrl}> and press the cloud icon.`);
       this.baseUrl = DOMAIN;
       this.hasProtectedChapters = true;
       this.directoryPath = "series";
-      this.chapterEndpoint = 1;
+      this.chapterEndpoint = 0;
       this.useListParameter = false;
       this.bypassPage = `${DOMAIN}/?p`;
       this.parser = new ShinigamiParser();
