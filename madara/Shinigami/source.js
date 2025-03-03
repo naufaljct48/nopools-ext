e630dfa4f11ec62db2aeda005b3ff9a32b68d09c
naufaljct48 <naufaljct48@gmail.com>
2025-03-03 06:12:06 +0000

    Deploying to gh-pages from @ naufaljct48/nopools-ext@23df3aa03a51fb4945c42eb22ddfdff0a3c2f00d 🚀

--- madara/Shinigami/source.js
+++ madara/Shinigami/source.js
@@ -1651,6 +1651,7 @@ var _Sources = (() => {
     getChildren: () => getChildren,
     getElementById: () => getElementById,
     getElements: () => getElements,
+    getElementsByClassName: () => getElementsByClassName,
     getElementsByTagName: () => getElementsByTagName,
     getElementsByTagType: () => getElementsByTagType,
     getFeed: () => getFeed,
@@ -3049,7 +3050,7 @@ var _Sources = (() => {
   }
   function find(test, nodes, recurse, limit) {
     const result = [];
-    const nodeStack = [nodes];
+    const nodeStack = [Array.isArray(nodes) ? nodes : [nodes]];
     const indexStack = [0];
     for (; ; ) {
       if (indexStack[0] >= nodeStack[0].length) {
@@ -3076,25 +3077,26 @@ var _Sources = (() => {
     return nodes.find(test);
   }
   function findOne(test, nodes, recurse = true) {
-    let elem = null;
-    for (let i = 0; i < nodes.length && !elem; i++) {
-      const node = nodes[i];
-      if (!isTag2(node)) {
-        continue;
-      } else if (test(node)) {
-        elem = node;
-      } else if (recurse && node.children.length > 0) {
-        elem = findOne(test, node.children, true);
+    const searchedNodes = Array.isArray(nodes) ? nodes : [nodes];
+    for (let i = 0; i < searchedNodes.length; i++) {
+      const node = searchedNodes[i];
+      if (isTag2(node) && test(node)) {
+        return node;
+      }
+      if (recurse && hasChildren(node) && node.children.length > 0) {
+        const found = findOne(test, node.children, true);
+        if (found)
+          return found;
       }
     }
-    return elem;
+    return null;
   }
   function existsOne(test, nodes) {
-    return nodes.some((checked) => isTag2(checked) && (test(checked) || existsOne(test, checked.children)));
+    return (Array.isArray(nodes) ? nodes : [nodes]).some((node) => isTag2(node) && test(node) || hasChildren(node) && existsOne(test, node.children));
   }
   function findAll(test, nodes) {
     const result = [];
-    const nodeStack = [nodes];
+    const nodeStack = [Array.isArray(nodes) ? nodes : [nodes]];
     const indexStack = [0];
     for (; ; ) {
       if (indexStack[0] >= nodeStack[0].length) {
@@ -3106,11 +3108,9 @@ var _Sources = (() => {
         continue;
       }
       const elem = nodeStack[0][indexStack[0]++];
-      if (!isTag2(elem))
-        continue;
-      if (test(elem))
+      if (isTag2(elem) && test(elem))
         result.push(elem);
-      if (elem.children.length > 0) {
+      if (hasChildren(elem) && elem.children.length > 0) {
         indexStack.unshift(0);
         nodeStack.unshift(elem.children);
       }
@@ -3171,6 +3171,9 @@ var _Sources = (() => {
   }
   function getElementsByTagName(tagName, nodes, recurse = true, limit = Infinity) {
     return filter(Checks["tag_name"](tagName), nodes, recurse, limit);
+  }
+  function getElementsByClassName(className, nodes, recurse = true, limit = Infinity) {
+    return filter(getAttribCheck("class", className), nodes, recurse, limit);
   }
   function getElementsByTagType(type, nodes, recurse = true, limit = Infinity) {
     return filter(Checks["tag_type"](type), nodes, recurse, limit);
@@ -16189,9 +16192,9 @@ Please go to the homepage of <${this.baseUrl}> and press the cloud icon.`);
   };
 
   // src/Shinigami/Shinigami.ts
-  var DOMAIN = "https://shinigami07.com";
+  var DOMAIN = "https://shinigami09.com";
   var ShinigamiInfo = {
-    version: getExportVersion("0.1.0"),
+    version: getExportVersion("0.1.1"),
     name: "Shinigami",
     description: `Extension that pulls manga from ${DOMAIN}`,
     author: "NaufalJCT48",
