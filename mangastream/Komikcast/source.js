53844499e80077acc8368707eddaf59bdbcc91eb
naufaljct48 <naufaljct48@gmail.com>
2025-03-03 06:34:00 +0000

    Deploying to gh-pages from @ naufaljct48/nopools-ext@c212bf07657a85b56981a8ab8da79c2372054d1a 🚀

--- mangastream/Komikcast/source.js
+++ mangastream/Komikcast/source.js
@@ -15597,7 +15597,7 @@ Please go to the homepage of <${this.baseUrl}> and press the cloud icon.`);
   // src/Komikcast/Komikcast.ts
   var DOMAIN = "https://komikcast02.com";
   var KomikcastInfo = {
-    version: getExportVersion("0.1.7"),
+    version: getExportVersion("0.1.8"),
     name: "Komikcast",
     description: `Extension that pulls manga from ${DOMAIN}`,
     author: "NaufalJCT48",
@@ -15625,7 +15625,7 @@ Please go to the homepage of <${this.baseUrl}> and press the cloud icon.`);
       this.homescreen_sections["latest_update"].selectorFunc = ($2, element) => $2("div.utao");
       this.homescreen_sections["latest_update"].titleSelectorFunc = ($2, element) => $2("div.luf h3", element).text().trim();
       this.homescreen_sections["latest_update"].subtitleSelectorFunc = ($2, element) => $2("div.luf ul li:first-child a", element).text().trim();
-      this.homescreen_sections["latest_update"].getViewMoreItemsFunc = (page) => `komik/page/${page}/?sortby=update`;
+      this.homescreen_sections["latest_update"].getViewMoreItemsFunc = (page) => `daftar-komik/page/${page}/?sortby=update`;
       this.homescreen_sections["popular_today"].enabled = true;
       this.homescreen_sections["popular_today"].selectorFunc = ($2, element) => $2(".swiper-slide");
       this.homescreen_sections["popular_today"].titleSelectorFunc = ($2, element) => $2("div.title", element).text().trim();
