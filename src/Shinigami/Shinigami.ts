99dddac0e10409e7024c3a7249c2e99ef577936e
naufal48 <naufaljct48@gmail.com>
2025-03-21 23:11:21 +0700

    refactor: update request and chapter details headers for improved compatibility
    Refactored the `createRequestObject` function to consolidate headers and added detailed headers for chapter page requests in `parseChapterDetails`. These changes ensure better compatibility with the Shinigami API and improve request handling.

--- src/Shinigami/Shinigami.ts
+++ src/Shinigami/Shinigami.ts
@@ -20,7 +20,7 @@ const CDN_URL = 'https://storage.shngm.id'
 const BASE_URL = 'https://app.shinigami.asia'
 
 export const ShinigamiInfo: SourceInfo = {
-    version: '1.0.7',
+    version: '1.0.8',
     name: 'Shinigami',
     icon: 'icon.png',
     author: 'NaufalJCT48',
--- src/Shinigami/ShinigamiHelper.ts
+++ src/Shinigami/ShinigamiHelper.ts
@@ -3,14 +3,21 @@ import { Request, HomeSection, PagedResults, Tag, TagSection } from '@paperback/
 const BASE_URL = 'https://app.shinigami.asia'
 
 export const createRequestObject = (requestObj: any): Request => {
+    const headers = {
+        'Accept': 'application/json',
+        'Origin': BASE_URL,
+        'DNT': '1',
+        'Sec-GPC': '1',
+        'Accept-Encoding': 'gzip, deflate, br',
+        'Accept-Language': 'en-US,en;q=0.9',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36'
+    }
+
     return App.createRequest({
         ...requestObj,
         headers: {
-            ...(requestObj.headers ?? {}),
-            'Accept': 'application/json',
-            'Origin': BASE_URL,
-            'DNT': '1',
-            'Sec-GPC': '1'
+            ...headers,
+            ...(requestObj.headers ?? {})
         }
     })
 }
