f580f2cd760e52008961d2b99059d1e0db6d0c9e
naufaljct48 <naufaljct48@gmail.com>
2025-03-22 03:54:38 +0000

    Deploying to gh-pages from @ naufaljct48/nopools-ext@b3f4544f45b96c3afb7fb2a6b61928a904d58117 🚀

--- mangastream/Shinigami/source.js
+++ mangastream/Shinigami/source.js
@@ -824,7 +824,7 @@ var _Sources = (() => {
   var API_URL = "https://api.shngm.io";
   var BASE_URL2 = "https://app.shinigami.asia";
   var ShinigamiInfo = {
-    version: "1.1.8",
+    version: "1.1.9",
     name: "Shinigami",
     icon: "icon.png",
     author: "NaufalJCT48",
@@ -932,6 +932,42 @@ var _Sources = (() => {
             type: import_types.HomeSectionType.featured,
             view_more: true
           })
+        },
+        {
+          request: createRequestObject({
+            url: `${API_URL}/v1/manga/list?type=mirror&page=1&page_size=15&is_update=true&sort=latest&sort_order=desc`,
+            method: "GET"
+          }),
+          section: App.createHomeSection({
+            id: "mirror",
+            title: "Mirror Project",
+            type: import_types.HomeSectionType.singleRowNormal,
+            view_more: true
+          })
+        },
+        {
+          request: createRequestObject({
+            url: `${API_URL}/v1/manga/list?format=manga&page=1&page_size=10&is_recommended=true`,
+            method: "GET"
+          }),
+          section: App.createHomeSection({
+            id: "manga",
+            title: "Recommended Manga",
+            type: import_types.HomeSectionType.singleRowNormal,
+            view_more: true
+          })
+        },
+        {
+          request: createRequestObject({
+            url: `${API_URL}/v1/manga/list?format=manhua&page=1&page_size=10&is_recommended=true`,
+            method: "GET"
+          }),
+          section: App.createHomeSection({
+            id: "manhua",
+            title: "Recommended Manhua",
+            type: import_types.HomeSectionType.singleRowNormal,
+            view_more: true
+          })
         }
       ];
       for (const section of sections) {
@@ -989,10 +1025,14 @@ var _Sources = (() => {
         params.push(`q=${encodeURIComponent(query.title)}`);
       }
       if (query.includedTags?.length) {
-        const genres = query.includedTags.filter((tag) => tag.id === "genres").map((tag) => tag.label);
-        const formats = query.includedTags.filter((tag) => tag.id === "formats").map((tag) => tag.label);
-        if (genres.length) params.push(`genre=${genres.join(",")}`);
-        if (formats.length) params.push(`format=${formats.join(",")}`);
+        const genres = query.includedTags.filter((tag) => tag.id === "genres").map((tag) => tag.id);
+        const formats = query.includedTags.filter((tag) => tag.id === "types").map((tag) => tag.id);
+        if (genres.length) {
+          params.push(`genre_include=${genres.join(",")}`);
+        }
+        if (formats.length) {
+          params.push(`format=${formats.join(",")}`);
+        }
       }
       const request = createRequestObject({
         url: `${API_URL}/v1/manga/list`,
@@ -1019,6 +1059,15 @@ var _Sources = (() => {
           break;
         case "featured":
           param = `?format=manhwa&page=${page}&page_size=30&is_recommended=true`;
+          break;
+        case "mirror":
+          param = `?type=mirror&page=${page}&page_size=30&is_update=true&sort=latest&sort_order=desc`;
+          break;
+        case "manga":
+          param = `?format=manga&page=${page}&page_size=30&is_recommended=true`;
+          break;
+        case "manhua":
+          param = `?format=manhua&page=${page}&page_size=30&is_recommended=true`;
           break;
         default:
           throw new Error(`Invalid homepage section id: ${homepageSectionId}`);
