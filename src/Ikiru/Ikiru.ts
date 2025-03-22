e50d0fbf217501a9273b94bc847f12712882bd74
naufal48 <naufaljct48@gmail.com>
2025-03-22 11:21:56 +0700

    feat: add Ikiru and Komiksin manga source extensions

---
+++ src/Ikiru/Ikiru.ts
@@ -0,0 +1,50 @@
+import {
+    BadgeColor,
+    ContentRating,
+    SourceInfo,
+    SourceIntents
+} from '@paperback/types'
+import {
+    BasicAcceptedElems,
+    CheerioAPI
+} from 'cheerio'
+import { AnyNode } from 'domhandler'
+
+import {
+    getExportVersion,
+    MangaStream
+} from '../MangaStream'
+
+const DOMAIN = 'https://ikiru.world'
+
+export const IkiruInfo: SourceInfo = {
+    version: getExportVersion('0.0.1'),
+    name: 'Ikiru',
+    description: `Extension that pulls manga from ${DOMAIN}`,
+    author: 'NaufalJCT48',
+    authorWebsite: 'http://github.com/NaufalJCT48',
+    icon: 'icon.png',
+    contentRating: ContentRating.MATURE,
+    websiteBaseURL: DOMAIN,
+    intents: SourceIntents.MANGA_CHAPTERS | SourceIntents.HOMEPAGE_SECTIONS | SourceIntents.CLOUDFLARE_BYPASS_REQUIRED | SourceIntents.SETTINGS_UI,
+    sourceTags: [
+    {
+        text: "Indonesia",
+        type: BadgeColor.GREY
+    },
+    ]
+}
+
+export class Ikiru extends MangaStream {
+    baseUrl: string = DOMAIN
+
+    override configureSections(): void {
+        this.homescreen_sections['new_titles'].enabled = false
+        this.homescreen_sections['top_alltime'].enabled = false
+        this.homescreen_sections['top_monthly'].enabled = false
+        this.homescreen_sections['top_weekly'].enabled = false
+        this.homescreen_sections['popular_today'].selectorFunc = ($: CheerioAPI, element: BasicAcceptedElems<AnyNode>) => $('div.bsx', $('h3:contains(Popular Today)')?.parent()?.next())
+        this.homescreen_sections['latest_update'].selectorFunc = ($: CheerioAPI, element: BasicAcceptedElems<AnyNode>) => $('div.utao', $('h3:contains(Latest Update)')?.parent()?.next())
+    }
+
+}
---
+++ src/Komiksin/Komiksin.ts
@@ -0,0 +1,67 @@
+import {
+    BadgeColor,
+    ContentRating,
+    SourceInfo,
+    SourceIntents
+} from '@paperback/types'
+import {
+    BasicAcceptedElems,
+    CheerioAPI
+} from 'cheerio'
+import { AnyNode } from 'domhandler'
+
+
+import {
+    getExportVersion,
+    MangaStream
+} from '../MangaStream'
+
+const DOMAIN = 'https://komiksin.id'
+
+export const KomiksinInfo: SourceInfo = {
+    version: getExportVersion('0.0.1'),
+    name: 'Komiksin',
+    description: `Extension that pulls manga from ${DOMAIN}`,
+    author: 'NaufalJCT48',
+    authorWebsite: 'http://github.com/NaufalJCT48',
+    icon: 'icon.png',
+    contentRating: ContentRating.EVERYONE,
+    websiteBaseURL: DOMAIN,
+    intents: SourceIntents.MANGA_CHAPTERS | SourceIntents.HOMEPAGE_SECTIONS | SourceIntents.CLOUDFLARE_BYPASS_REQUIRED | SourceIntents.SETTINGS_UI,
+    sourceTags: [
+        {
+            text: "Indonesia",
+            type: BadgeColor.GREY
+        },
+    ]
+}
+
+export class Komiksin extends MangaStream {
+
+    baseUrl: string = DOMAIN
+
+    override manga_tag_selector_box = 'div.seriestugenre'
+
+    override configureSections(): void {
+        this.homescreen_sections['new_titles'].enabled = false
+        this.homescreen_sections['popular_today'].selectorFunc = ($: CheerioAPI, element: BasicAcceptedElems<AnyNode>) => $('div.bsx', $('h2:contains(Terpopuler Hari Ini)')?.parent()?.next())
+        this.homescreen_sections['latest_update'].selectorFunc = ($: CheerioAPI, element: BasicAcceptedElems<AnyNode>) => $('div.uta', $('h2:contains(Rilisan Terbaru)')?.parent()?.next())
+        this.homescreen_sections['top_alltime'].enabled = false
+        this.homescreen_sections['top_monthly'].enabled = false
+        this.homescreen_sections['top_weekly'].enabled = false
+    }
+    override dateMonths = {
+        january: 'Januari',
+        february: 'Februari',
+        march: 'Maret',
+        april: 'April',
+        may: 'Mei',
+        june: 'Juni',
+        july: 'Juli',
+        august: 'Agustus',
+        september: 'September',
+        october: 'Oktober',
+        november: 'November',
+        december: 'Desember'
+    }
+}
