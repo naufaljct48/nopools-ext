1d4c0c3ae57cf8507140cc21693b6993280494de
Nopools <naufaljct48@gmail.com>
2025-10-18 10:09:16 +0700

    chore: update WestManga extension version to 0.0.3

--- src/WestManga/WestManga.ts
+++ src/WestManga/WestManga.ts
@@ -24,7 +24,7 @@ import {
 const DOMAIN = 'https://westmanga.me'
 
 export const WestMangaInfo: SourceInfo = {
-    version: getExportVersion('0.0.2'),
+    version: getExportVersion('0.0.3'),
     name: 'WestManga',
     description: `Extension that pulls manga from ${DOMAIN}`,
     author: 'NaufalJCT48',
