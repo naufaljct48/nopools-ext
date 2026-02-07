8fa3bd170f5c9c3c424e01f152d8602802198a51
naufaljct48 <naufaljct48@gmail.com>
2026-02-07 18:52:22 +0000

    Deploying to gh-pages from @ naufaljct48/nopools-ext@853f30d6707e0ee6ae2c1d139e169cebd7ac8710 🚀

--- mangastream/Kiryuu/source.js
+++ mangastream/Kiryuu/source.js
@@ -15874,7 +15874,7 @@ ${additionalInfo.join(" \u2022 ")}`;
   // src/Kiryuu/Kiryuu.ts
   var WEBSITE_BASE2 = "https://kiryuu03.com";
   var KiryuuInfo = {
-    version: "2.0.1",
+    version: "2.0.2",
     name: "Kiryuu",
     icon: "icon.png",
     author: "NaufalJCT48",
