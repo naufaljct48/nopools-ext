d1a91f4763599171bb87e0a3fdbd5f2827e72ff2
naufaljct48 <naufaljct48@gmail.com>
2026-02-08 03:14:49 +0000

    Deploying to gh-pages from @ naufaljct48/nopools-ext@873fcd89190e5732e729f2a1042e15737fd00cb5 🚀

--- mangastream/Kiryuu/source.js
+++ mangastream/Kiryuu/source.js
@@ -15874,7 +15874,7 @@ ${additionalInfo.join(" \u2022 ")}`;
   // src/Kiryuu/Kiryuu.ts
   var WEBSITE_BASE2 = "https://kiryuu03.com";
   var KiryuuInfo = {
-    version: "2.1.2",
+    version: "2.1.3",
     name: "Kiryuu",
     icon: "icon.png",
     author: "NaufalJCT48",
@@ -16066,6 +16066,20 @@ ${additionalInfo.join(" \u2022 ")}`;
         results,
         metadata: hasMore ? { page: page + 1 } : void 0
       });
+    }
+    async getCloudflareBypassRequestAsync() {
+      return App.createRequest({
+        url: `${WEBSITE_BASE2}/`,
+        method: "GET",
+        headers: {
+          "referer": `${WEBSITE_BASE2}/`,
+          "origin": `${WEBSITE_BASE2}/`,
+          "user-agent": await this.requestManager.getDefaultUserAgent()
+        }
+      });
+    }
+    getMangaShareUrl(mangaId) {
+      return `${WEBSITE_BASE2}/manga/${mangaId}`;
     }
   };
   return __toCommonJS(Kiryuu_exports);
