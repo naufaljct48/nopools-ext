de421bb905e3292f043cc37e48750d9dfe64240e
Nopools <naufaljct48@gmail.com>
2026-05-10 02:19:08 +0700

    chore: bump versions for DoujinDesu and KomikTap
    - Update DoujinDesu version from 3.0.0 to 5.0.0
    - Update KomikTap version from 0.0.4 to 3.0.0
    - Align version numbers with recent feature and enhancement updates

--- src/DoujinDesu/DoujinDesu.ts
+++ src/DoujinDesu/DoujinDesu.ts
@@ -41,7 +41,7 @@ const MONTHS: Record<string, number> = {
 }
 
 export const DoujinDesuInfo: SourceInfo = {
-    version: '3.0.0',
+    version: '5.0.0',
     name: 'DoujinDesu',
     icon: 'icon.png',
     author: 'NaufalJCT48',
--- src/KomikTap/KomikTap.ts
+++ src/KomikTap/KomikTap.ts
@@ -19,7 +19,7 @@ import {
 const DOMAIN = 'https://komiktap.info'
 
 export const KomikTapInfo: SourceInfo = {
-    version: getExportVersion('0.0.4'),
+    version: getExportVersion('3.0.0'),
     name: 'KomikTap',
     description: `Extension that pulls manga from ${DOMAIN}`,
     author: 'NaufalJCT48',
